34de4562ec249e158ce43f23ea2e211e338d9790
Zapstereo <fernandezzap@gmail.com>
2021-06-19 22:00:25 +0100

    had a fiddle with notes

--- Assets/Scripts/Zap/RhythmInitialise.cs
+++ Assets/Scripts/Zap/RhythmInitialise.cs
@@ -24,17 +24,18 @@ public class RhythmInitialise : MonoBehaviour
                     );
                     */
         float offset = -4f;
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("It's", 42+offset, lanes[0]); }, 32+offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("a", 42.25f+offset, lanes[1]); }, 32.25f+offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("beautiful", 42.5f+offset, lanes[2]); }, 32.5f+offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("day", 43f+offset, lanes[0]); }, 33f+offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("outside", 43.75f + offset, lanes[0]); }, 33.75f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("birds", 44.25f + offset, lanes[1]); }, 34.25f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("are", 44.5f + offset, lanes[2]); }, 34.5f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("singing", 45.0f + offset, lanes[0]); }, 35.0f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("flowers", 45.250f + offset, lanes[1]); }, 35.25f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("are", 45.5f + offset, lanes[2]); }, 35.5f + offset);
-        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("blooming", 45.750f + offset, lanes[0]); }, 35.75f + offset);
+        float leadTime = 5f;
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("It's", 42+offset, lanes[0]); }, 42+offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("a", 42.25f+offset, lanes[1]); }, 42.25f+offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("beautiful", 42.5f+offset, lanes[2]); }, 42.5f+offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("day", 43f+offset, lanes[0]); }, 43f+offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("outside", 43.75f + offset, lanes[0]); }, 43.75f + offset -leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("birds", 44.25f + offset, lanes[1]); }, 44.25f + offset - leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("are", 44.5f + offset, lanes[2]); }, 44.5f + offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("singing", 45.0f + offset, lanes[0]); }, 45.0f + offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("flowers", 45.250f + offset, lanes[1]); }, 45.25f + offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("are", 45.5f + offset, lanes[2]); }, 45.5f + offset-leadTime);
+        Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("blooming", 45.750f + offset, lanes[0]); }, 45.75f + offset-leadTime);
         //Rythm.RythmEngine.Instance.QueueActionAtExplicitBeat(() => { noteSpawner.SpawnNote("dad", 44, lanes[1]); }, 34);
 
     }
